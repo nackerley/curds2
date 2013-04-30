5bf7bb5a1be598fa2d4b5df77df924cf84ffabe2
Mark Williams <mark.c.will@gmail.com>
2013-04-30 13:58:23 -0700

    Finished version 1.0, execute calls Dbptr methods, inline doc almost complete

--- psycods2.py
+++ psycods2.py
@@ -10,7 +10,7 @@ from antelope.datascope import Dbptr, dbALL, dbNULL, dbINVALID
 
 apilevel     = "2.0"      # 1.0 or 2.0
 threadsafety = 0          # Playing it safe (datascope??)
-paramstyle   = "format"   # use %s style for now
+paramstyle   = None       # N/A right now, execute uses Dbptr API
 
 class Error(exceptions.StandardError):
     pass
@@ -61,13 +61,16 @@ class Cursor(object):
     Methods (DBAPI standard)
     -------
     close() : Close the connection
-    callproc(procname, params=[]) : Access to Dbptr functions
-    execute(operation, params=[]) : Call dbprocess string command
+    execute(operation, params=[]) : Call Dbptr method
     executemany(operation, param_seq=[]) : Execute same operation multiple times
     fetchone() : Get record of current pointer
     fetchmany(size=cursor.arraysize) : Get multiple records from current pointer on
     fetchall() : Get all records from current pointer to end
     
+    Extension methods
+    -----------------
+    scroll(record, mode="relative") : Move cursor pointer to a record
+
     Built-ins
     ---------
     __iter__ : Cursor is a generator which can be iterated over
@@ -152,87 +155,88 @@ class Cursor(object):
         Inputs
         ------
         dbptr    : antelope.datascope.Dbptr
-        **kwargs : keyword args passed to dblookup
+        **kwargs : keyword args, where
+            -> if a cursor attribute, set attribute value
+            -> remaining kwargs passed to dblookup
         
         """
         self._dbptr = Dbptr(dbptr)
-        if kwargs:
-            self._dbptr = self._dbptr.lookup(**kwargs)
+        # Attributes
+        for k in kwargs.keys():
+            if hasattr(self, k):
+                self.__setattr__(k, kwargs.pop(k))
+        # inherit row_factory from Connection if not set on creation
+        if self.row_factory is None and self.connection:
+            self.row_factory = self.connection.row_factory
+        # pass anything else to dblookup
+        self._dbptr = self._dbptr.lookup(**kwargs)
+
     
     def __iter__(self):
+        """Generator, yields a row from 0 to rowcount"""
         for self._dbptr.record in xrange(self.rowcount):
             yield self._fetch()
-    
-    def _none_if_eq(self, v1, v2):
-        """Return v1 if v1 != v2, None if v1 == v2"""
-        if v1 == v2:
-            return None
-        else:
-            return v1
-    
-    def _map_ds_nulls(self, row, nullrow):
-        """For two seq, replace items in row with None if they == nullrow items"""
-        return tuple([self._none_if_eq(r, nullrow[n]) for n, r in enumerate(row)])
     
     def _fetch(self):
         """Pull out a row from DB and increment pointer"""
         fields = [d[0] for d in self.description]
         row = self._dbptr.getv(*fields)
-        if self.CONVERT_NULL:
-            nullrow = self._nullptr.getv(*fields)
-            row = self._map_ds_nulls(row, nullrow)
+        if self.CONVERT_NULL:    
+            row = tuple([row[n] != null and row[n] or None for n, null in enumerate(self._nullptr.getv(*fields))])
         if self.row_factory:
             row = self.row_factory(self, row)
         self._dbptr.record += 1
         return row
-        
+
     def close(self):
         self._dbptr.close()
 
-    def callproc(self, procname, params=[]):
+    def execute(self, operation, params=[]):
         """
-        Call stored procedure
+        Execute Datascope database command
+
+        Because Datascope doesn't have an exposed 'language', and
+        most of the functionality is already available through the
+        Dbptr API, this is just an attempt at standardizing these calls.
+
         
         Inputs
         ------
-        procname : name of a Dbptr method
-        params   : sequence of valid parameters for given method
+        operation : name of a Dbptr method
+        params    : sequence or mapping of valid parameters for given method
         
         Returns
         -------
-        Depends on command, anything returning a Dbptr is treated as an
-        'execute', anything else is returned
+        Depends on command, anything returning a Dbptr modifies the cursor
+        and is available through the 'fetch*' methods or by iterating, 
+        and returns the number of rows, anything else is returned directly.
         
         Notes
         -----
-        This is a hacky way to get at Datascope functions, most importantly,
-        the 'query', but will work with 'addv', even 'process', etc...
+        This is a hacky way to get at Datascope functions, originally done
+        through 'callproc', but this should be the main method, so... 
+        
+        The Dbptr API already converts basic types, but not everything, 
+        the NULL implementatiion is a mess, and times are just floats, so,
+        in the future, could check the type and implement the DBAPI types
+        by say, converting any datetime objects to the float stamp expected.
         
         """
-        if hasattr(self._dbptr, procname):
-            proc = getattr(self._dbptr, procname)
-            result = proc(*params)
-            if isinstance(result, Dbptr):
-                self._dbptr = result
-                return self.rowcount
-            else:
-                return result
+        if not hasattr(self._dbptr, operation):
+            raise Exception("No such command available: " + operation)
+            
+        proc = getattr(self._dbptr, operation)
+        if isinstance(params, dict):
+            result = proc(**params)
         else:
-            raise Exception("No such command available: " + procname)
+            result = proc(*params)
+        
+        if isinstance(result, Dbptr):
+            self._dbptr = result
+            return self.rowcount
+        else:
+            return result
     
-    def execute(self, operation, params=[]):
-        """
-        Execute (send commands to dbprocess)
-        
-        For API compatibility -  it basically runs like this:
-        Dbptr.process( "operation" % (*params) )
-        
-        """
-        # parse the substitutions unsecurely for now
-        command_str = operation % tuple(params)
-        self._dbptr = self._dbptr.process(command_str)
-        return self.rowcount
-
     def executemany(self, operation, param_seq=[]):
         for params in param_seq:
             rc = self.execute(operation, params)
@@ -240,14 +244,25 @@ class Cursor(object):
         
     def fetchone(self):
         """
-        Return one row
+        Return one row fro current pointer.
+
+        Returns
+        -------
+        tuple or row_factory-generated row
+
+        If CONVERT_NULL is True, any value equal to its NULL value
+        will be a python None.
         
+        Notes
+        -----
         If the 'dbALL' record is there, just start at first one
         also, rollover to 0 if at the end
         
         """
         if self.rownumber == dbALL or self.rownumber == self.rowcount:
             self._dbptr.record = 0
+        if not 0 <= self.rownumber < self.rowcount:
+            raise ProgrammingError("Not a valid record number: "+ str(self.rownumber))
         return self._fetch()
 
     def fetchmany(self, size=None):
@@ -258,6 +273,10 @@ class Cursor(object):
         ------
         size : int of number of records to return (self.arraysize)
         
+        Returns
+        -------
+        list of tuples or row_factory-generated rows
+
         Notes
         -----
         If no 'size' given, uses the 'arraysize' attribute
@@ -268,15 +287,20 @@ class Cursor(object):
         """
         if size is None:
             size = self.arraysize
-        if not 0 <= self.rownumber < self.rowcount:
-            raise ProgrammingError("Not a valid record number: "+ str(self.rownumber))
         end = self.rownumber + size
         if end > self.rowcount:
             end = self.rowcount
         return [self.fetchone() for self._dbptr.record in xrange(self.rownumber, end)]
             
     def fetchall(self):
-        """Return the rest of the rows"""
+        """
+        Return the rest of the rows
+
+        Returns
+        -------
+        list of tuples or row_factory-generated rows
+        
+        """
         return self.fetchmany(size=self.rowcount)
         
     def scroll(self, value, mode='relative'):
@@ -298,7 +322,7 @@ class Cursor(object):
             recnum = value
         else:
             raise ProgrammingError("Invalid mode: " + mode)
-        if 0 <= value < self.rowcount:
+        if 0 <= recnum < self.rowcount:
             self._dbptr.record = recnum
         else:
             raise IndexError("Produces an index out of range")
@@ -313,7 +337,7 @@ class Connection(object):
 
     row_factory = None
 
-    def __init__(self, database, **kwargs):
+    def __init__(self, database, perm='r', **kwargs):
         """
         Open a connection to a Datascope database
         
@@ -323,8 +347,11 @@ class Connection(object):
         perm     : str of permissions
     
         """
-        self._dbptr = Dbptr(database, **kwargs)
-
+        self._dbptr = Dbptr(database, perm=perm)
+        for k in kwargs.keys():
+            if hasattr(self, k):
+                self.__setattr__(k, kwargs.pop(k))
+        
     def close(self):
         self._dbptr.close()
 
@@ -344,9 +371,9 @@ class Connection(object):
         (Any kwargs are passed to dblookup)
         
         """
-        return Cursor(self._dbptr, **kwargs)
+        return Cursor(self._dbptr, connection=self, **kwargs)
         
-def connect(dsn, perm='r'):
+def connect(dsn, perm='r', **kwargs):
     """
     Return a Connection object to a Datascope database
     
@@ -357,7 +384,7 @@ def connect(dsn, perm='r'):
         
     
     """
-    return Connection(dsn, perm=perm)
+    return Connection(dsn, perm=perm, **kwargs)
 
 #--- Utilities -------------------------------------------------------#
 # Row factory functions, based on sqlite3's DBAPI implementation
