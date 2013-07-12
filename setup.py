0776f144b8a63fdc02343b5b9b7ed0a211645066
Mark Williams <mark.c.will@gmail.com>
2013-07-12 13:33:50 -0700

    Name change to folder for release

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@
 # setup.py file for installing
 #
 from numpy.distutils.core import setup, Extension
-import sys, os
 
-s_args = {'name'         : 'psycods2',
+s_args = {'name'         : 'curds2',
           'version'      : '0.4.0',
           'description'  : 'DBAPI2 compatible module for Datascope',
           'author'       : 'Mark Williams',
           'url'          : 'https//github.com/markcwill',
-          'packages'     : ['psycods2'],
+          'packages'     : ['curds2'],
 }
 
 # Go
