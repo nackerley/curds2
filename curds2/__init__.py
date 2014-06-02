b22281b32d84007935af142ba44a95e7a57f956f
Mark <mark.c.will@gmail.com>
2014-06-02 15:39:41 -0700

    Took out auto module import

--- curds2/__init__.py
+++ curds2/__init__.py
@@ -3,12 +3,5 @@
 #
 """
 DBAPI2.0 module for Datascope
-
-Contains the API and several row factories
-
 """
-__version__ = '0.5.5'
-
-from dbapi2 import *
-from rows import NamedTupleRow, OrderedDictRow, UTCOrdDictRow, SQLValuesRow
-
+__version__ = '0.6.0'
