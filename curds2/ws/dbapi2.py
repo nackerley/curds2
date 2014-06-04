5f64c63b6fd5e27d243983095de489b845042923
Mark <mark.c.will@gmail.com>
2014-06-04 12:39:07 -0700

    DBAPI2 client works now

--- curds2/ws/dbapi2.py
+++ curds2/ws/dbapi2.py
@@ -4,8 +4,9 @@ curds2.dbapi2 module for Datascope
 
 Uses the base python wrappers
 """
-import urlparse
+#import urlparse
 import urllib2
+import json
 
 from curds2.api.core import *
 from curds2.api.base import *
@@ -27,12 +28,75 @@ ROWID    = DBAPITypeObject(dbDBPTR)
 
 # implement Connection, Cursor, etc
 class Cursor(BaseCursor):
-    pass
+    """
+    Stub Cursor class for a remote client
+    """
+    _request = {'jsonrpc': '2.0'}
+    _headers = {'content-type': 'application/json'}
+    _rows = []
+    
+    description = []
+    
+    def __init__(self, *args, **kwargs):
+        """Constructor"""
+        super(Cursor, self).__init__(**kwargs)
+        
+        self.dsn = self.connection.dsn
+        
+        # Attributes
+        for k, v in kwargs.items():
+            if hasattr(self, k):
+                self.__setattr__(k, v)
 
+    @staticmethod
+    def _convert_dt(value, type_code):
+        if type_code == DATETIME and value is not None and isinstance(value, float):
+            return TimestampFromTicks(value)
+        return value
+    
+    def _fetch(self):
+        n = self._record
+        desc = self.description
+        row = self._rows[n]
+        self._record += 1
+        if self.CONVERT_DATETIME:
+            row = [self._convert_dt(row[n], d[1]) for n, d in enumerate(desc)]
+        return self.row_factory(self, row)
+
+    @property
+    def rowcount(self):
+        return len(self._rows)
+    
+    def execute(self, operation, params=[]):
+        """
+        Call server at a URL and get result
+        """
+        curs_settings = ('CONVERT_NULL',)  # to forward to server Cursor
+        curs_params = dict([(p, getattr(self, p)) for p in curs_settings])
+        rpc_params = {'commands': params, 'cursor': curs_params}
+        self._request.update({'method': operation, 'params': rpc_params, 'id': 1})
+        req = urllib2.Request(self.dsn, json.dumps(self._request), self._headers)
+        rep = urllib2.urlopen(req)
+        j = rep.read()
+        rep.close()
+        result = json.loads(j)
+        if 'result' in result:
+            result = result.get('result')
+        else:
+            err = result.get('error')
+            raise DatabaseError(": ".join([err['type'], err['message']]))
+        self.description = result.get('description')
+        self._rows = result.get('rows')
+        return self.rowcount
 
 class Connection(BaseConnection):
-    pass
+    """
+    Connection class for remote
+    
+    Stub to hold data for Cursor.execute
+    """
+    cursor_factory = Cursor
 
 
 def connect(dsn, *args, **kwargs):
-    pass
+    return Connection(dsn, *args, **kwargs)
